6d534f45825cbee22d439e7a12998934d8ee7d29
David <davbetm@gmail.com>
2020-05-18 23:39:14 -0500

    Primeros ejercicios de data visualization

---
+++ data_visualization/2_Using_axes.py
@@ -0,0 +1,41 @@
+# Import matplotlib.pyplot
+import matplotlib.pyplot as plt
+import numpy as np
+
+""" Graficar varias series de datos en un mismo plot pero diferentes gráficas.
+Usando unidades relativas para definir ancho y largo.
+"""
+
+year = np.array([1970, 1971, 1972, 1973, 1974, 1975, 1976, 1977, 1978, 1979, 1980,
+       1981, 1982, 1983, 1984, 1985, 1986, 1987, 1988, 1989, 1990, 1991,
+       1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002,
+       2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011]
+    )
+
+physical_sciences = np.array(
+    [13.8, 14.9, 14.8, 16.5, 18.2, 19.1, 20. ,21.3, 22.5,23.7, 24.6,
+       25.7, 27.3, 27.6, 28. , 27.5, 28.4, 30.4, 29.7, 31.3, 31.6, 32.6,
+       32.6, 33.6, 34.8, 35.9, 37.3, 38.3, 39.7, 40.2, 41. , 42.2, 41.1,
+       41.7, 42.1, 41.6, 40.8, 40.7, 40.7, 40.7, 40.2, 40.1]
+    )
+
+computer_science = np.array([13.8, 14.9, 14.8, 16.5, 18.2, 19.1, 20. , 21.3, 22.5,
+        23.7, 24.6, 25.7, 27.3, 27.6, 28. , 27.5, 28.4, 30.4, 29.7, 31.3, 31.6, 32.6,
+        32.6, 33.6, 34.8, 35.9, 37.3, 38.3, 39.7, 40.2, 41. , 42.2, 41.1,
+        41.7, 42.1, 41.6, 40.8, 40.7, 40.7, 40.7, 40.2, 40.1]
+    )
+
+# Create plot axes for the first line plot
+plt.axes([0.05, 0.05, 0.425, 0.9])
+
+# Plot in blue the % of degrees awarded to women in the Physical Sciences
+plt.plot(year, physical_sciences, color='blue')
+
+# Create plot axes for the second line plot
+plt.axes([0.525,0.05,0.425,0.9])
+
+# Plot in red the % of degrees awarded to women in Computer Science
+plt.plot(year, computer_science, color='red')
+
+# Display the plot
+plt.show()
---
+++ data_visualization/3_Using_subplot.py
@@ -0,0 +1,46 @@
+# Import matplotlib.pyplot
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+"""The command plt.axes() requires a lot of effort to use well because the
+coordinates of the axes need to be set manually. A better alternative is to use
+plt.subplot() to determine the layout automatically.
+"""
+
+year = np.array([1970, 1971, 1972, 1973, 1974, 1975, 1976, 1977, 1978, 1979, 1980,
+       1981, 1982, 1983, 1984, 1985, 1986, 1987, 1988, 1989, 1990, 1991,
+       1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002,
+       2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011]
+    )
+
+physical_sciences = np.array(
+    [13.8, 14.9, 14.8, 16.5, 18.2, 19.1, 20. ,21.3, 22.5,23.7, 24.6,
+       25.7, 27.3, 27.6, 28. , 27.5, 28.4, 30.4, 29.7, 31.3, 31.6, 32.6,
+       32.6, 33.6, 34.8, 35.9, 37.3, 38.3, 39.7, 40.2, 41. , 42.2, 41.1,
+       41.7, 42.1, 41.6, 40.8, 40.7, 40.7, 40.7, 40.2, 40.1]
+    )
+
+computer_science = np.array([13.8, 14.9, 14.8, 16.5, 18.2, 19.1, 20. , 21.3, 22.5,
+        23.7, 24.6, 25.7, 27.3, 27.6, 28. , 27.5, 28.4, 30.4, 29.7, 31.3, 31.6, 32.6,
+        32.6, 33.6, 34.8, 35.9, 37.3, 38.3, 39.7, 40.2, 41. , 42.2, 41.1,
+        41.7, 42.1, 41.6, 40.8, 40.7, 40.7, 40.7, 40.2, 40.1]
+    )
+
+# Create a figure with 1x2 subplot and make the left subplot active
+plt.subplot(1,2,1)
+
+# Plot in blue the % of degrees awarded to women in the Physical Sciences
+plt.plot(year, physical_sciences, color='blue')
+plt.title('Physical Sciences')
+
+# Make the right subplot active in the current 1x2 subplot grid
+plt.subplot(1,2,2)
+
+# Plot in red the % of degrees awarded to women in Computer Science
+plt.plot(year, computer_science, color='red')
+plt.title('Computer Science')
+
+# Use plt.tight_layout() to improve the spacing between subplots
+plt.tight_layout()
+plt.show()
